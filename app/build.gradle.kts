1ac0cd78a312d0a13739349d6350336554f9970f
Tamas Tegzes <tegzes.tamas@gmail.com>
2021-04-13 20:13:21 +0200

    Add test switch implementation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,6 +29,13 @@ dependencies {
     // This dependency is used by the application.
     implementation("com.google.guava:guava:29.0-jre")
 
+    implementation("io.javalin:javalin:3.13.5")
+
+    implementation("org.slf4j", "slf4j-simple", "1.7.25")
+    implementation("org.slf4j", "slf4j-api", "1.7.25")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.12.2")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.12.2")
+
     // Use the Kotlin test library.
     testImplementation("org.jetbrains.kotlin:kotlin-test")
 
